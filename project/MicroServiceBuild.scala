14cd6fd391525f72260e38cba6a611563214510e
yasar.acar <yasar.acar@accenture.com>
2016-11-28 12:03:52 +0000

    IHT-4215 Update to latest play 2.3 libraries

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -17,7 +17,6 @@
 import sbt._
 
 object MicroServiceBuild extends Build with MicroService {
-  import scala.util.Properties.envOrElse
 
   val appName = "iht"
 
@@ -28,28 +27,22 @@ private object AppDependencies {
   import play.PlayImport._
   import play.core.PlayVersion
 
-  private val playMicroServiceVersion = "6.23.0"
-  private val playHealthVersion = "0.7.0"
-  private val hmrctest = "1.9.0"
-  private val httpCachingClientVersion = "5.3.0"
-
   val compile = Seq(
     "uk.gov.hmrc" %% "play-reactivemongo" % "4.8.0",
     ws,
     "uk.gov.hmrc" %% "microservice-bootstrap" % "4.4.0",
-    "uk.gov.hmrc" %% "play-authorisation" % "3.3.0",
-    "uk.gov.hmrc" %% "play-config" % "2.0.1",
-    "uk.gov.hmrc" %% "play-url-binders" % "1.0.0",
-    "uk.gov.hmrc" %% "play-scheduling" % "1.1.0",
-    "uk.gov.hmrc" %% "play-health" % playHealthVersion,
-    "uk.gov.hmrc" %% "play-json-logger" % "1.0.0",
+    "uk.gov.hmrc" %% "play-authorisation" % "3.4.0",
+    "uk.gov.hmrc" %% "play-config" % "2.1.0",
+    "uk.gov.hmrc" %% "play-url-binders" % "1.1.0",
+    "uk.gov.hmrc" %% "play-scheduling" % "3.0.0",
+    "uk.gov.hmrc" %% "play-health" % "1.1.0",
+    "uk.gov.hmrc" %% "play-json-logger" % "2.1.1",
+    "uk.gov.hmrc" %% "http-verbs" % "5.0.0",
+    "uk.gov.hmrc" %% "http-caching-client" % "5.6.0",
+    "uk.gov.hmrc" %% "domain" % "3.7.0",
     "com.kenshoo" %% "metrics-play" % "2.3.0_0.1.6",
     "com.codahale.metrics" % "metrics-graphite" % "3.0.2",
-    "uk.gov.hmrc" %% "hmrctest" % hmrctest,
-    "com.github.fge" % "json-schema-validator" % "2.2.6",
-    "uk.gov.hmrc" %% "http-verbs" % "5.0.0",
-    "uk.gov.hmrc" %% "http-caching-client" % httpCachingClientVersion,
-    "uk.gov.hmrc" %% "domain" % "3.7.0"
+    "com.github.fge" % "json-schema-validator" % "2.2.6"
   )
 
   trait TestDependencies {
@@ -61,11 +54,11 @@ private object AppDependencies {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
         "uk.gov.hmrc" %% "reactivemongo-test" % "1.6.0" % scope,
+        "uk.gov.hmrc" %% "hmrctest" % "1.9.0" % scope,
         "org.scalatest" %% "scalatest" % "2.2.2" % scope,
         "org.scalacheck" %% "scalacheck" % "1.12.2" % scope,
         "org.pegdown" % "pegdown" % "1.4.2" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-        "uk.gov.hmrc" %% "hmrctest" % hmrctest % scope,
         "com.github.fge" % "json-schema-validator" % "2.2.6"
       )
     }.test
