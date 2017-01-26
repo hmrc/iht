1e768ef687b92e7cf8bfaa21336b04d488a1dac2
yasar.acar <yasar.acar@accenture.com>
2017-01-26 11:45:08 +0000

    IHT-4397 update dependencies in project/MicroServiceBuild.scala

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -28,19 +28,19 @@ private object AppDependencies {
   import play.core.PlayVersion
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "play-reactivemongo" % "4.8.0",
+    "uk.gov.hmrc" %% "play-reactivemongo" % "5.1.0",
     ws,
-    "uk.gov.hmrc" %% "microservice-bootstrap" % "4.4.0",
-    "uk.gov.hmrc" %% "play-authorisation" % "3.4.0",
-    "uk.gov.hmrc" %% "play-config" % "2.1.0",
-    "uk.gov.hmrc" %% "play-url-binders" % "1.1.0",
-    "uk.gov.hmrc" %% "play-scheduling" % "3.0.0",
-    "uk.gov.hmrc" %% "play-health" % "1.1.0",
-    "uk.gov.hmrc" %% "play-json-logger" % "2.1.1",
-    "uk.gov.hmrc" %% "http-verbs" % "5.0.0",
-    "uk.gov.hmrc" %% "http-caching-client" % "5.6.0",
-    "uk.gov.hmrc" %% "domain" % "3.7.0",
-    "uk.gov.hmrc" %% "play-graphite" % "2.0.0",
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "5.8.0",
+    "uk.gov.hmrc" %% "play-authorisation" % "4.2.0",
+    "uk.gov.hmrc" %% "play-config" % "3.0.0",
+    "uk.gov.hmrc" %% "play-url-binders" % "2.0.0",
+    "uk.gov.hmrc" %% "play-scheduling" % "4.0.0",
+    "uk.gov.hmrc" %% "play-health" % "2.0.0",
+    "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
+    "uk.gov.hmrc" %% "http-verbs" % "6.2.0",
+    "uk.gov.hmrc" %% "http-caching-client" % "6.1.0",
+    "uk.gov.hmrc" %% "domain" % "4.0.0",
+    "uk.gov.hmrc" %% "play-graphite" % "3.1.0",
     "com.github.fge" % "json-schema-validator" % "2.2.6"
   )
 
@@ -52,8 +52,8 @@ private object AppDependencies {
   object Test {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
-        "uk.gov.hmrc" %% "reactivemongo-test" % "1.6.0" % scope,
-        "uk.gov.hmrc" %% "hmrctest" % "1.9.0" % scope,
+        "uk.gov.hmrc" %% "reactivemongo-test" % "2.0.0" % scope,
+        "uk.gov.hmrc" %% "hmrctest" % "2.2.0" % scope,
         "org.scalatest" %% "scalatest" % "2.2.2" % scope,
         "org.scalacheck" %% "scalacheck" % "1.12.2" % scope,
         "org.pegdown" % "pegdown" % "1.4.2" % scope,
