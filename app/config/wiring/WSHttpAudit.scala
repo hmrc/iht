649ba8bb1cf8404c94042521f570c7f081de9166
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2018-05-31 09:46:09 +0100

    Update copyrights

--- app/config/wiring/WSHttpAudit.scala
+++ app/config/wiring/WSHttpAudit.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,4 +30,3 @@ trait Hooks extends HttpHooks with HttpAuditing {
 
 trait WSHttp extends HttpGet with WSGet with HttpPut with WSPut with HttpPost with WSPost with HttpDelete with WSDelete with Hooks with AppName
 object WSHttp extends WSHttp
-
